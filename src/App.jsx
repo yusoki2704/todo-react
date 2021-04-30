4816d400e85be6f700122db5ddc5c21541e3039b
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-01 07:39:02 +0900

    fix styles.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,29 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div></div>
-      <input placeholder="TODOを入力" />
-      <button>追加</button>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>あああああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>ううううう</li>
             <button>戻す</button>
           </div>
