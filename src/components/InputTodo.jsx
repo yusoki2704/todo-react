276282882342ab1dadb8429f72620dddab6dc71b
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-03 07:28:59 +0900

    include css in InputTodo.jsx

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,16 @@
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
