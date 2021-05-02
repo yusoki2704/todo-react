ed98b7b1f74cc8ad482bf05311da8c943c0d8020
yusoki2704 <71821574+yusoki2704@users.noreply.github.com>
2021-05-03 07:36:25 +0900

    add max size incompleteTodos

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTodoは5個までです。Todoを消化してください。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
